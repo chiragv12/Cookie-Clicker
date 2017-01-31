20a77a83f01b4a2149081d92868f015baf02d400
Chirag <chiragv00@gmail.com>
2017-01-31 11:35:07 -0500

    v

--- app/src/main/java/com/chig/cookieclicker/MainActivity.java
+++ app/src/main/java/com/chig/cookieclicker/MainActivity.java
@@ -29,6 +29,8 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+
+
         imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
